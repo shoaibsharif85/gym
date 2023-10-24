28b83fb9a23b3815bcfe6bb7a12eda9a6e44ce6f
Shoaib Sharif <shoaibsharif85@gmail.com>
2023-10-23 22:07:09 -0700

    g

--- src/component/Trainer.jsx
+++ src/component/Trainer.jsx
@@ -3,7 +3,7 @@ import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg";
 import img3 from "../assets/img3.jpg";
 
-import { BsFacebook, BsInstagram, BsTwitter } from 'react-icons/bs'
+
 
 
 const Trainers = () => {
@@ -16,27 +16,21 @@ const Trainers = () => {
                     <img src={img1} alt="" className="w-[200px]  rounded-md mt-10 mx-10" />
                     <h1 className="ml-[90px] text-white my-5 font-bold text-3xl ">Anotly</h1>
                     <div className="flex text-center text-white ml-[80px] text-3xl">
-                        <BsFacebook className=" " />
-                        <BsInstagram className="mx-3" />
-                        <BsTwitter />
+                      
                     </div>
                 </div>
                 <div className="w-[280px] h-[430px]   bg-[#504747] rounded-2xl">
                     <img src={img2} alt="" className="w-[200px]  rounded-md mt-10 mx-10" />
                     <h1 className="ml-[90px] text-white my-5 font-bold text-3xl ">Anotly</h1>
                     <div className="flex text-center text-white ml-[80px] text-3xl">
-                        <BsFacebook className=" " />
-                        <BsInstagram className="mx-3" />
-                        <BsTwitter />
+                      
                     </div>
                 </div>
                 <div className="w-[280px] h-[430px]  bg-[#504747] rounded-2xl">
                     <img src={img3} alt="" className="w-[200px]  rounded-md mt-10 mx-10" />
                     <h1 className="ml-[90px] text-white my-5 font-bold text-3xl ">Anotly</h1>
                     <div className="flex text-center text-white ml-[80px] text-3xl">
-                        <BsFacebook className=" " />
-                        <BsInstagram className="mx-3" />
-                        <BsTwitter />
+                     
                     </div>
                 </div>
             </div>
